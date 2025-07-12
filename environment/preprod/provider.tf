3702db2ca69c4b340f9b73e2ed96c586241e1003
Achinta <achintadutta1997@gmail.com>
2025-07-12 15:51:04 +0530

    code has been added

---
+++ environment/preprod/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.0"
+    }
+  }
+
+  backend "azurerm"{
+
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "289da116-5358-4835-8897-9e1db2cde3d2"
+}
